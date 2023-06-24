cd9adff5127acfd6cc06e30493f4128c5abe680c
adolfosonchiche <sonadolfo36@gmail.com>
2023-06-23 22:13:15 -0600

    mostrar data obtenido de la peticion http en pantalla - usuarios

--- demo-001/src/components/Usuarios.tsx
+++ demo-001/src/components/Usuarios.tsx
@@ -8,32 +8,53 @@ export const Usuarios = () => {
 
     useEffect(() => {
         //llamado al API  por axios tambien se puede hacer con fetch
-        ReqResApi.get<ReqRespListado>('/user')
-        .then( resp => {
-            console.log(resp.data.data)
-        })
-        .catch( console.log )
+        ReqResApi.get<ReqRespListado>('/users')
+            .then(resp => {
+                //console.log(resp.data.data)
+                setUsuarios(resp.data.data) //establecer los usuarios
+            })
+            .catch(console.log)
 
     }, [])
 
-  return (
-    <>
-    <h3>Usuarios</h3>
+    const renderItem = (usuario: Usuario) => {
+        return (
+            <tr key={usuario.id}>
+                <td><img src={usuario.avatar} alt={usuario.first_name} style={{
+                    width: 30,
+                    borderRadius: 100
+                }} /></td>
+                <td>{usuario.first_name} {usuario.last_name}</td>
+                <td> {usuario.email}</td>
+            </tr>
+        )
+    }
 
-    <table className="table">
-        <thead>
-        <tr>
-            <th>Avatar</th>
-            <th>Nombre</th>
-            <th>email</th>
-        </tr>
-        </thead>
-        <tbody>
+    return (
+        <>
+            <h3>Usuarios</h3>
 
-        </tbody>
-    </table>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre</th>
+                        <th>email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        //usuarios.map(usuario => renderItem(usuario))
+                        usuarios.map(renderItem)
 
+                    }
+                </tbody>
+            </table>
 
-    </>
-  )
+            <button className="btn btn-primary">
+                siguientes
+            </button>
+
+        </>
+    )
 }
